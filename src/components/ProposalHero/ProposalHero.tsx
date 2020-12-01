2b23e84371cd0f99e7e0e57d7216ddad5b13f850
Dheeraj Manjunath <dheerajmanju1@gmail.com>
2020-11-30 23:16:03 -0500

    lint

--- src/components/ProposalHero/ProposalHero.tsx
+++ src/components/ProposalHero/ProposalHero.tsx
@@ -5,8 +5,13 @@ import VoteMeter from 'components/VoteMeter'
 import ProposalStatusBadge from 'components/ProposalStatusBadge'
 import { Proposal, Outcome, Vote, Address, Status } from 'types'
 import Button, { ButtonType } from 'components/Button'
-import { leftPadZero, formatShortAud, formatWei, getDate } from 'utils/format'
-import { getHumanReadableTime } from 'utils/format'
+import {
+  leftPadZero,
+  formatShortAud,
+  formatWei,
+  getDate,
+  getHumanReadableTime
+} from 'utils/format'
 import { ReactComponent as IconThumbUp } from 'assets/img/iconThumbUp.svg'
 import { ReactComponent as IconThumbDown } from 'assets/img/iconThumbDown.svg'
 import ConfirmTransactionModal from 'components/ConfirmTransactionModal'
