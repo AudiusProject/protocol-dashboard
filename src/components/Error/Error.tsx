e0ebe640644d4a1f4907f6771567b94be77584ea
Raymond Jacobson <ray@audius.co>
2020-12-16 13:26:38 -0800

    Fix initial vote meter state

--- src/components/Error/Error.tsx
+++ src/components/Error/Error.tsx
@@ -7,7 +7,13 @@ import styles from './Error.module.css'
 
 const DEFAULT_ERROR_TEXT = 'Incomplete Data'
 
-const Error = ({ text = DEFAULT_ERROR_TEXT, className }: { text?: string, className?: string }) => {
+const Error = ({
+  text = DEFAULT_ERROR_TEXT,
+  className
+}: {
+  text?: string
+  className?: string
+}) => {
   return (
     <div className={clsx(styles.error, className)}>
       <Tooltip text={text}>
